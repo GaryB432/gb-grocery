2a9a263cdd2271234e85321f1f5df00bf2b71d4b
Gary Bortosky <garybort@gmail.com>
2016-10-14 21:18:48 -0500

    tweak randomString function

--- src/app/shared/utilities.ts
+++ src/app/shared/utilities.ts
@@ -9,16 +9,23 @@ import {
     AppInfo
 } from "../shared";
 
+// http://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript#10727155
+function randomString(length: number, chars: string): string {
+    let mask: string = "";
+    /* tslint:disable:curly */
+    if (chars.indexOf("a") > -1) mask += "abcdefghijklmnopqrstuvwxyz";
+    if (chars.indexOf("A") > -1) mask += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (chars.indexOf("#") > -1) mask += "0123456789";
+    if (chars.indexOf("!") > -1) mask += "~`!@#$%^&*()_+-={}[]:';\"<>?,./|\\";
+    let result: string = "";
+    for (let i: number = length; i > 0; --i) result += mask[Math.floor(Math.random() * mask.length)];
+    /* tslint:enable:curly */
+    return result;
+}
+
 export class Utilities {
     public static makeid(): string {
-        let text: string = "";
-        const possible: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-        for (let i: number = 0; i < 5; i++) {
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-
-        return text;
+        return randomString(5, "a#");
     }
     public static dtoToItem(dto: IItem): Item {
         const newItem: Item = new Item();
@@ -31,7 +38,7 @@ export class Utilities {
     public static dtoToStore(dto: IDtoStore): Store {
         const newStore: Store = new Store(dto.id, dto.name);
         newStore.placeId = dto.place_id;
-        newStore.icon = "http://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png";
+        newStore.icon = "https://maps.gstatic.com/mapfiles/place_api/icons/shopping-71.png";
         newStore.vicinity = dto.vicinity;
         newStore.checkouts = [];
         return newStore;
