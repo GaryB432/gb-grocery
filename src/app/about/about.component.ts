97b269b597072f2c0ce6275c5a548cd4c26a99a8
Gary Bortosky <garybort@gmail.com>
2016-10-16 01:02:06 -0500

    add rekey feature

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
-import { IDtoAppInfo } from "../shared/interfaces";
+import { IDtoAppInfo, IItem, IDtoStore } from "../shared/interfaces";
+import { Utilities } from "../shared/utilities";
 import { DataIoService } from "../shared/data.io.service";
 
 @Component({
@@ -37,9 +38,52 @@ export class AboutComponent implements OnInit {
     public replaceAppInfoForever(): void {
         try {
             const dto: IDtoAppInfo = JSON.parse(this.jsonInfo);
-            this.doReplace(dto).then((done: boolean) => done && this.router.navigateByUrl("home"));
+            this.doReplace(this.rekey(dto))
+                .then((done: boolean) => done && this.router.navigateByUrl("home"));
         } catch (e) {
             alert(e);
         }
+    }
+
+    private rekey(dto: IDtoAppInfo): IDtoAppInfo {
+        type Map<T> = { [ndx: string]: T };
+        type StoreMap = Map<IDtoStore>;
+        type ItemMap = Map<IItem>;
+
+        const storeMap: StoreMap = dto.stores.reduce<StoreMap>((pv: StoreMap, cv: IDtoStore) => {
+            pv[cv.id] = {
+                id: Utilities.makeStoreId(),
+                name: cv.name,
+                place_id: cv.place_id,
+                vicinity: cv.vicinity
+            };
+            return pv;
+        }, {});
+
+        const itemMap: ItemMap = dto.items.reduce<ItemMap>((pv: ItemMap, cv: IItem) => {
+            pv[cv.id] = {
+                id: Utilities.makeItemId(),
+                name: cv.name,
+                needed: cv.needed
+            };
+            return pv;
+        }, {});
+
+        return {
+            stores: Object.keys(storeMap).map((k: string) => storeMap[k]),
+            items: Object.keys(itemMap).map((k: string) => itemMap[k]),
+            checkouts: dto.checkouts.map(c => {
+                return {
+                    storeId: storeMap[c.storeId].id,
+                    isoDate: c.isoDate,
+                    pickups: c.pickups.map(p => {
+                        return {
+                            itemId: itemMap[p.itemId].id,
+                            aisle: p.aisle
+                        };
+                    })
+                };
+            })
+        };
     }
 }
