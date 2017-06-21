d8dc1127216ca6c4bbbdaf08d272db19ce1a944e
Gary Bortosky <garybort@gmail.com>
2017-06-21 12:20:43 -0500

    move web.config

--- deploy-azure.cmd
+++ deploy-azure.cmd
@@ -1,6 +1,7 @@
 call ng build -prod --sourcemaps false --delete-output-path true
 robocopy dist ../gb-grocery-iis /MIR /XD .git
 ping 127.0.0.1 -n 6 > nul
+copy web.config ..\gb-grocery-iis /Y
 pushd ..\gb-grocery-iis
 git add .
 git commit -m "new version"
