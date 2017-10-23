c6334a0dd92d5870ef8ba89a5697855cc678448d
Gary Bortosky <garybort@gmail.com>
2017-10-23 12:50:35 -0500

    update nginx

--- deploy-azure.cmd
+++ deploy-azure.cmd
@@ -2,3 +2,4 @@ call npm run build
 docker build -t gb-grocery:latest .
 docker tag gb-grocery:latest garyb.azurecr.io/garyb432/gb-grocery:latest
 docker push garyb.azurecr.io/garyb432/gb-grocery:latest
+az webapp restart -n gb-grocery -g gbg-rg
