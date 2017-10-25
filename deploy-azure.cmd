a028e2757e6eba1c396ae9ec53e550abc4d40fd2
Gary Bortosky <garybort@gmail.com>
2017-10-25 10:10:21 -0500

    tweak azure deployment

--- deploy-azure.cmd
+++ deploy-azure.cmd
@@ -1,5 +1,6 @@
+REM docker run -it azuresdk/azure-cli-python:latest bash
 call npm run build
 docker build -t gb-grocery:latest .
-docker tag gb-grocery:latest garyb.azurecr.io/garyb432/gb-grocery:latest
-docker push garyb.azurecr.io/garyb432/gb-grocery:latest
+docker tag gb-grocery:latest garyb432/gb-grocery:latest
+docker push garyb432/gb-grocery:latest
 az webapp restart -n gb-grocery -g gbg-rg
