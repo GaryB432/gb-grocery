339e6883fd28727d5ce6cf0d5a5920bf10bb1832
Gary Bortosky <garybort@gmail.com>
2018-09-09 10:53:51 -0500

    add prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'gbg-root',
+  styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'gb-grocery';
