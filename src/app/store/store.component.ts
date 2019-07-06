e42147c262578e49e5c727542b8badfc80ff1490
Gary Bortosky <garybort@gmail.com>
2019-07-06 10:05:26 -0500

    fix store select

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -107,8 +107,7 @@ export class StoreComponent implements OnInit {
     pickup.picked = !pickup.picked;
   }
 
-  public changeStore(_event?: any): void {
-    console.log(_event);
+  public changeStore(_placeID?: string): void {
     const nbs = this.selectedStore;
     if (!nbs) {
       throw new Error('changing to null store');
