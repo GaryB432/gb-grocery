2c1824fca4d3ffe75081168eecea783b746a7337
Gary Bortosky <garybort@gmail.com>
2020-10-08 21:29:42 -0500

    try build info again

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
-
-// import { FirebaseApp } from '@firebase/app-types';
-// import { FirebaseAuth } from '@firebase/auth-types';
-// import { FirebaseDatabase } from '@firebase/database-types';
-// import { FirebaseMessaging } from '@firebase/messaging-types';
-// import { FirebaseStorage } from '@firebase/storage-types';
-// import { FirebaseFirestore } from '@firebase/firestore-types';
-
 import * as firebase from 'firebase/app';
+import { environment } from '../../environments/environment';
 import { DataIOService } from '../shared/data/data-io.service';
 import * as Dto from '../shared/data/dto';
 
@@ -25,6 +18,52 @@ export class AboutComponent implements OnInit {
   public displayName?: string;
   public email?: string;
   public photoURL?: string;
+  public environmentKeys: string[] = [
+    'INPUT_AZURE_STATIC_WEB_APPS_API_TOKEN',
+    'INPUT_REPO_TOKEN',
+    'INPUT_ACTION',
+    'INPUT_APP_BUILD_COMMAND',
+    'INPUT_APP_LOCATION',
+    'INPUT_API_LOCATION',
+    'INPUT_APP_ARTIFACT_LOCATION',
+    'INPUT_API_BUILD_COMMAND',
+    'INPUT_ROUTES_LOCATION',
+    'HOME',
+    'GITHUB_JOB',
+    'GITHUB_REF',
+    'GITHUB_SHA',
+    'GITHUB_REPOSITORY',
+    'GITHUB_REPOSITORY_OWNER',
+    'GITHUB_RUN_ID',
+    'GITHUB_RUN_NUMBER',
+    'GITHUB_RETENTION_DAYS',
+    'GITHUB_ACTOR',
+    'GITHUB_WORKFLOW',
+    'GITHUB_HEAD_REF',
+    'GITHUB_BASE_REF',
+    'GITHUB_EVENT_NAME',
+    'GITHUB_SERVER_URL',
+    'GITHUB_API_URL',
+    'GITHUB_GRAPHQL_URL',
+    'GITHUB_WORKSPACE',
+    'GITHUB_ACTION',
+    'GITHUB_EVENT_PATH',
+    'GITHUB_PATH',
+    'GITHUB_ENV',
+    'RUNNER_OS',
+    'RUNNER_TOOL_CACHE',
+    'RUNNER_TEMP',
+    'RUNNER_WORKSPACE',
+    'ACTIONS_RUNTIME_URL',
+    'ACTIONS_RUNTIME_TOKEN',
+    'ACTIONS_CACHE_URL',
+  ];
+
+  public env: {
+    firebase: any;
+    azure: { [key: string]: string };
+    production: boolean;
+  } = environment;
 
   constructor(
     private afAuth: AngularFireAuth,
